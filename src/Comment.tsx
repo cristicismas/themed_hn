ae1504e217e353f0a6971d0c4423e4619cae51b9
Cristi Cismas <cristicismas99@gmail.com>
2019-02-26 23:02:14 +0200

    Add functionality to collapse comments

--- src/Comment.tsx
+++ src/Comment.tsx
@@ -16,6 +16,12 @@ const StyledComment = styled.div`
 
     return level * 50 + 'px';
   }};
+
+  .collapse-button {
+    background-color: transparent;
+    border: 0;
+    cursor: pointer;
+  }
 `;
 
 import Comments from './Comments';
@@ -37,23 +43,44 @@ interface IProps {
   level: number;
 }
 
-class Comment extends Component<IProps, object> {
+interface IState {
+  collapsed: boolean;
+}
+
+class Comment extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      collapsed: false
+    };
+  }
+
   render() {
     const { comment, level } = this.props;
+    const { collapsed } = this.state;
     // Convert unix time to js' Date.
     const commentTime = new Date(comment.time * 1000);
+
+    const collapseSign = collapsed ? '+' : '-';
 
     return (
       <div>
         <StyledComment level={level} className='comment' id={comment.id.toString()}>
-          <p
-            className='comment-text'
-            dangerouslySetInnerHTML={{ __html: comment.deleted ? '[deleted]' : comment.text }}
-          />
-          <div>by <Link to={`profile/${comment.by}`}>{comment.by}</Link> <TimeAgo date={commentTime} /></div>
+          {!collapsed ? (
+            <p
+              className='comment-text'
+              dangerouslySetInnerHTML={{ __html: comment.deleted ? '[deleted]' : comment.text }}
+              id={comment.id.toString()}
+            />
+          ) : null}
+          <div>
+            by <Link to={`profile/${comment.by}`}>{comment.by}</Link> <TimeAgo date={commentTime} />{' '}
+            <button onClick={() => { this.setState({ collapsed: !this.state.collapsed }) }} className='collapse-button'>[{collapseSign}]</button>
+          </div>
         </StyledComment>
         <hr />
-        <Comments comments={comment.kids ? comment.kids : []} level={level + 1} />
+        {!collapsed ? <Comments comments={comment.kids ? comment.kids : []} level={level + 1} /> : null}
       </div>
     );
   }
